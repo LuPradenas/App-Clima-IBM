6307d2ed514ef6111c45ca6dbf7d9657ebfe7af2
LuPradenas <lucianapradenas3@gmail.com>
2021-11-10 15:19:19 -0300

    realice la vista de ciudades y agregue el modal para la busqueda

---
+++ utils/colors.js
@@ -0,0 +1,18 @@
+export const COLORS = {
+    primary: "#162C39",
+    primaryVariant: "#e7eff6",
+    secondary: "#5c7f90",
+    secondaryVariant: "#007AFF",
+    secondaryVariantGradient: "#52c1f6",
+  
+    onPrimary: "#eeeeee",
+    onPrimaryHint: "#777777",
+  
+    white: "#fff",
+    black: "#000",
+    yellow: "yellow",
+    orange: "orange",
+  
+    error: "#b0253c",
+    separator: "#859699",
+  };
