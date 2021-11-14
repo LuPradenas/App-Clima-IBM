d958414e6f847684713174f22851548d8b4bc5e8
Alejandro Calgaro <59174208+alecalgaro@users.noreply.github.com>
2021-11-14 12:04:31 -0300

    Se modificó la vista Quienes Somos

---
+++ components/Cards.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, ScrollView, StyleSheet, Image, Linking } from 'react-native';
+import { Text, Card, Button, Icon } from 'react-native-elements';
+
+//type CardsComponentsProps = {};
+
+const Cards = (props) => {
+
+  const {name, image, description, linkedin} = props;
+  
+  return (
+
+        <View style={styles.container}>
+          
+          <Card>
+            <Card.Title>{name}</Card.Title>
+            <Card.Divider />
+ 
+              <Card.Image
+                source={require(`../img/${image}`)}
+                style={{ height: 200,  borderRadius: 50 }}
+                resizeMode="center"   // center o contain
+              />
+
+            <Text style={{ marginVertical: 10 }}>
+              {description}
+            </Text>
+
+            <Button
+              icon={
+                <Icon
+                  name="sc-linkedin"
+                  type="evilicon"
+                  color="#ffffff"
+                  iconStyle={{ marginRight: 5 }}
+                />
+              }
+              
+              title="Contactar"
+              onPress={() => Linking.openURL(linkedin)}
+            />
+
+          </Card>
+        </View>
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10
+  },
+
+});
+
+export default Cards;
--- views/QuienesSomos.js
+++ views/QuienesSomos.js
@@ -1,12 +1,100 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import { Tab, Text, TabView } from 'react-native-elements';
+import Cards from '../components/Cards';
 
 const QuienesSomos = () => {
-    return (
-        <View>
-            <Text>Quienes somos</Text>
-        </View>
-    )
-}
+  const [index, setIndex] = useState(0);
 
-export default QuienesSomos
+  return (
+    <>
+      <Tab
+        value={index}
+        onChange={(e) => setIndex(e)}
+        indicatorStyle={{
+          backgroundColor: 'white',
+          height: 3,
+        }}
+        variant="primary">
+        <Tab.Item
+          title="Nosotros"
+          titleStyle={{ fontSize: 12 }}
+          icon={{ name: '', type: 'ionicon', color: 'white' }} 
+        />
+        <Tab.Item
+          title="Solución"
+          titleStyle={{ fontSize: 12 }}
+          icon={{ name: '', type: 'ionicon', color: 'white' }}
+        />
+        <Tab.Item
+          title="Diseño"
+          titleStyle={{ fontSize: 12 }}
+          icon={{ name: '', type: 'ionicon', color: 'white' }}
+        />
+      </Tab>
+
+      <TabView value={index} onChange={setIndex} animationType="spring">
+        <TabView.Item style={{ width: '100%' }}>
+          <ScrollView>
+            <Cards
+              name="IARA BAYA"
+              image='foto-iara.webp'
+              description="Esta es la descripción"
+              linkedin="https://www.linkedin.com/in/"
+            />
+
+            <Cards
+              name="ALEJANDRO CALGARO"
+              image='foto-ale.webp'
+              description={`Edad: 25 años.
+Ciudad: Chajari, Entre Ríos.
+Analista en Informática Aplicada y Estudiante de Ingeniería en informática.`}
+              linkedin="https://www.linkedin.com/in/alejandrocalgaro/"
+            />
+
+            <Cards
+              name="LUCIANA PRADENAS"
+              image='foto-luciana.webp'
+              description="Esta es la descripción"
+              linkedin="https://www.linkedin.com/in/"
+            />
+
+            <Cards
+              name="PABLO RIVERA"
+              image='foto-pablo.webp'
+              description="Esta es la descripción"
+              linkedin="https://www.linkedin.com/in/"
+            />
+          </ScrollView>
+        </TabView.Item>
+
+        <TabView.Item style={styles.solution}>
+          <Text>Acá iría la explicación de la solución</Text>
+        </TabView.Item>
+
+        <TabView.Item style={styles.design}>
+          <>
+            <Text>Se realizó el wireframe de cada pantalla en Balsamiq y luego los mockups en Figma.</Text>
+            <Text>Acá iría un botón que nos lleve al link a Balsamiq y otro al link de Figma</Text>
+          </>
+        </TabView.Item>
+      </TabView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  solution: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  design: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default QuienesSomos;
